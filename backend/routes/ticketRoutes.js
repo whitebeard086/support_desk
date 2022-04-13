c4db0e2a85295c5800102bae8a55cada2a2289d8
unknown <whitebeard086@gmail.com>
2022-04-13 08:23:57 +0100

    setup ticket model and routes in the backend

---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { getTickets, createTicket } = require("../controllers/ticketController");
+const router = express.Router();
+
+
+const { protect } = require("../middleware/authMiddleware");
+
+router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+module.exports = router;
