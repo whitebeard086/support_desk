41d0d46e34b711694242ed2428cb6621bb95816a
unknown <whitebeard086@gmail.com>
2022-04-13 23:32:49 +0100

    implement create ticket functionality and add Back button component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,4 @@
+export { default as BackButton } from "./BackButton";
 export { default as Header } from "./Header";
 export { default as PrivateRoute } from "./PrivateRoute";
 export { default as Spinner } from "./Spinner";
--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,19 +1,48 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { createTicket, reset } from "../features/tickets/ticketSlice";
+import { Spinner, BackButton } from "../components";
 
 const NewTicket = () => {
   const { user } = useSelector(state => state.auth);
+  const { isLoading, isError, isSuccess, message } = useSelector(state => state.ticket);
+
   const [name] = useState(user.name);
   const [email] = useState(user.email);
   const [product, setProduct] = useState("iPhone");
   const [description, setDescription] = useState("");
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess) {
+      dispatch(reset());
+      navigate("/tickets");
+    }
+
+    dispatch(reset());
+  }, [dispatch, isError, isSuccess, message, navigate]);
+
   const onSubmit = e => {
     e.preventDefault();
+    dispatch(createTicket({ product, description }));
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
+      <BackButton url="/" />
       <section className="heading">
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
@@ -53,7 +82,7 @@ const NewTicket = () => {
               onChange={e => setDescription(e.target.value)}></textarea>
           </div>
           <div className="form-group">
-              <button className="btn btn-block">Submit</button>
+            <button className="btn btn-block">Submit</button>
           </div>
         </form>
       </section>
